e8f514e54bea8bf1ad74c7fb6cbf44710229cd88
femtoepic <femtoepic@gmail.com>
2025-01-02 03:56:22 +0300

    Update iframe_deobfuscated.js
    more clean

--- iframe_deobfuscated.js
+++ iframe_deobfuscated.js
@@ -1,190 +1,80 @@
-(() => {
-    let v = {
-        sWQnJ: function(z, S) {
-            return z !== S;
-        },
-        fdagl: "dxqOo",
-        Mlriu: "REmTJ",
-        OByzl: function(z, S) {
-            return z !== S;
-        },
-        QoFUW: "RSzHZ",
-        cBako: "VasUF",
-        lkwvm: function(z, S) {
-            return z === S;
-        },
-        QNMXL: "jDMqM",
-        lDVPS: "GCGkt",
-        dHbLN: "xClLI",
-        afReH: "oLwjJ",
-        MipFw: function(z, S) {
-            return z + S;
-        },
-        npcyl: function(z, S) {
-            return z + S;
-        },
-        uDvgh: "no message",
-        zdUsg: "no stack",
-        rPXjO: "unknown",
-        JwuUw: function(z, S, H) {
-            return z(S, H);
-        },
-        TWyFV: "HTMLElement.prototype",
-        CFzaW: "innerText",
-        lygcC: "onload",
-        BpYFh: function(z, S, H) {
-            return z(S, H);
-        },
-        VpKeZ: "onerror",
-        EJtFe: function(z, S, H) {
-            return z(S, H);
-        },
-        Debfe: "onmousedown",
-        AFOdN: "onmouseup",
-        RVjLP: "onmousemove",
-        lSiSp: "onclick",
-        vsfMh: "Element.prototype",
-        jEkrF: "innerHTML",
-        gxmip: function(z, S, H) {
-            return z(S, H);
-        },
-        NGBZQ: "HTMLScriptElement.prototype",
-        iXPUv: "src",
-        eDUvR: "HTMLImageElement.prototype",
-        OHHOw: function(z, S, H) {
-            return z(S, H);
-        },
-        joewr: "HTMLLinkElement.prototype",
-        sotuj: "href",
-        YcRYf: "type",
-        XNgXS: function(z, S, H) {
-            return z(S, H);
-        },
-        zkjEd: "rel",
-        feebd: "MessagePort.prototype",
-        xlUDY: "onmessage",
-        LACCr: function(z, S) {
-            return z - S;
-        },
-        MaODM: function(z, S) {
-            return z + S;
-        },
-        oZZwR: "/report/",
-        BeKRL: "frame",
-        NWovF: "POST",
-        wDsKJ: "(((.+)+)+)+$",
-        PfRcr: function(z, S) {
-            return z != S;
-        },
-        LOkFx: "object",
-        BupTE: "HCF",
-        YGveY: function(z, S) {
-            return z !== S;
-        },
-        WVWRu: "QzUiI",
-        ggAcC: "DgneI",
-        juQPD: function(z, S) {
-            return z(S);
-        },
-        TVcHN: function(z, S) {
-            return z + S;
-        },
-        vyhKq: "/rpc/",
-        aYSSX: function(z, S) {
-            return z + S;
-        },
-        GfRbR: function(z, S) {
-            return z - S;
-        }
-    };
-    onmessage = async z => {
-        let S = {
-            Xvadf: v.wDsKJ
-        };
-        if (!v.PfRcr(v.LOkFx, typeof z.data) && !v.OByzl(z.data.rid, v.MaODM(v.BupTE, jid))) {
-            onmessage = () => {};
-            let H = null;
-            try {
-                if (v.YGveY(v.WVWRu, v.ggAcC)) {
-                    let g = Date.now(), A = await v.juQPD(fetch, v.TVcHN(v.vyhKq, eid)).then(F => F.arrayBuffer()), E = Date.now(), O = new Uint8Array(A), W = v.MipFw(new Uint32Array(O.buffer, O.byteOffset, 4)[0], 4), I = new TextDecoder().decode(new Uint8Array(O.buffer, v.aYSSX(O.byteOffset, W), v.GfRbR(O.byteLength, W)));
-                    H = Date.now();
-                    var G = O.slice().buffer, q = Date.now();
-                    v.juQPD(Function, I)()(I, -2031046560029716628n, [ z.data.data, g, E, q ], w, G);
-                } else try {
-                    var P, R = E.split(/\./);
-                    let Z = s;
-                    for (P of R) Z = Z[P];
-                    return O.getOwnPropertyDescriptor(Z, R) ? "1" : "0";
-                } catch (u) {
-                    return "" + u;
-                }
-            } catch (R) {
-                R.__hcl || v.juQPD(w, R);
+onmessage = async z => {
+    if ("object" == typeof z.data && z.data.rid === "HCF" + jid) {
+        onmessage = () => {};
+        let H = null;
+        try {
+            {
+                let g = Date.now(), A = await fetch("/rpc/" + eid).then(F => F.arrayBuffer()), E = Date.now(), O = new Uint8Array(A), W = new Uint32Array(O.buffer, O.byteOffset, 4)[0] + 4, I = new TextDecoder().decode(new Uint8Array(O.buffer, O.byteOffset + W, O.byteLength - W));
+                H = Date.now();
+                var G = O.slice().buffer, q = Date.now();
+                Function(I)()(I, -2031046560029716628n, [ z.data.data, g, E, q ], w, G);
             }
-            async function w(g) {
-                let A = {
-                    UGYhc: function(E, O) {
-                        return v.sWQnJ(E, O);
-                    },
-                    XeyYa: v.fdagl,
-                    lnVwD: v.Mlriu,
-                    aQonh: function(E, O) {
-                        return v.OByzl(E, O);
-                    },
-                    QPWgR: v.QoFUW,
-                    cWvit: v.cBako,
-                    iFelN: function(E, O) {
-                        return v.lkwvm(E, O);
-                    },
-                    MDykv: v.QNMXL,
-                    ikWbd: v.lDVPS
-                };
-                if (!v.OByzl(v.dHbLN, v.afReH)) return S.toString().search(WZbdBs.Xvadf).toString().constructor(H).search(WZbdBs.Xvadf);
-                console.error(g);
-                var E = g?.message || g?.stack ? v.MipFw(v.npcyl(g?.message ?? v.uDvgh, "\n"), g?.stack ?? v.zdUsg) : g?.toString?.() ?? v.rPXjO;
-                if (!/^Load failed|^Failed to fetch|^The operation was aborted|^NetworkError when attempting to fetch|^отменено/.test(E)) {
-                    var O = navigator.userAgent, W = (P, u) => {
-                        if (!A.UGYhc(A.XeyYa, A.lnVwD)) return "" + z;
-                        try {
-                            var Y;
-                            if (A.aQonh(A.QPWgR, A.cWvit)) {
-                                var B, y = P.split(/\./);
-                                let U = window;
-                                for (B of y) U = U[B];
-                                return Object.getOwnPropertyDescriptor(U, u) ? "1" : "0";
-                            }
-                            return Y = H.apply(z, arguments), w = null, Y;
-                        } catch (Y) {
-                            if (A.iFelN(A.MDykv, A.ikWbd)) {
-                                var d;
-                                let T = w;
-                                for (d of Y.split(/\./)) T = T[d];
-                                return g.getOwnPropertyDescriptor(T, A) ? "1" : "0";
-                            }
-                            return "" + Y;
+        } catch (R) {
+            R.__hcl || w(R);
+        }
+        async function w(g) {
+            let A = {
+                UGYhc: function(E, O) {
+                    return E !== O;
+                },
+                XeyYa: "dxqOo",
+                lnVwD: "REmTJ",
+                aQonh: function(E, O) {
+                    return E !== O;
+                },
+                QPWgR: "RSzHZ",
+                cWvit: "VasUF",
+                iFelN: function(E, O) {
+                    return E === O;
+                },
+                MDykv: "jDMqM",
+                ikWbd: "GCGkt"
+            };
+            console.error(g);
+            var E = g?.message || g?.stack ? (z => z + (g?.stack ?? "no stack"))((g?.message ?? "no message") + "\n") : g?.toString?.() ?? "unknown";
+            if (!/^Load failed|^Failed to fetch|^The operation was aborted|^NetworkError when attempting to fetch|^отменено/.test(E)) {
+                var O = navigator.userAgent;
+                let I = [ (W = (P, u) => {
+                    if (!A.UGYhc(A.XeyYa, A.lnVwD)) return "" + z;
+                    try {
+                        var Y;
+                        if (A.aQonh(A.QPWgR, A.cWvit)) {
+                            var B, y = P.split(/\./);
+                            let U = window;
+                            for (B of y) U = U[B];
+                            return Object.getOwnPropertyDescriptor(U, u) ? "1" : "0";
                         }
-                    };
-                    let I = [ v.JwuUw(W, v.TWyFV, v.CFzaW), v.JwuUw(W, v.TWyFV, v.lygcC), v.BpYFh(W, v.TWyFV, v.VpKeZ), v.EJtFe(W, v.TWyFV, v.Debfe), v.BpYFh(W, v.TWyFV, v.AFOdN), v.EJtFe(W, v.TWyFV, v.RVjLP), v.BpYFh(W, v.TWyFV, v.lSiSp), v.EJtFe(W, v.vsfMh, v.jEkrF), v.gxmip(W, v.NGBZQ, v.iXPUv), v.BpYFh(W, v.eDUvR, v.iXPUv), v.OHHOw(W, v.joewr, v.sotuj), v.gxmip(W, v.joewr, v.YcRYf), v.XNgXS(W, v.joewr, v.zkjEd), v.BpYFh(W, v.feebd, v.xlUDY) ], G = bid, q = null;
-                    var W = window?.performance?.memory, W = (W && (q = {
-                        totalJSHeapSize: W.totalJSHeapSize,
-                        usedJSHeapSize: W.usedJSHeapSize,
-                        jsHeapSizeLimit: W.jsHeapSizeLimit
-                    }), v.LACCr(Date.now(), window.gid)), Z = H && v.LACCr(Date.now(), H);
-                    await v.EJtFe(fetch, v.MaODM(v.oZZwR, G), {
-                        body: JSON.stringify({
-                            esid: E,
-                            uaid: O,
-                            wkid: I,
-                            muid: q,
-                            ltid: W,
-                            dtid: Z,
-                            ctid: v.BeKRL
-                        }),
-                        method: v.NWovF,
-                        keepalive: !0
-                    });
-                }
+                        return Y = H.apply(z, arguments), w = null, Y;
+                    } catch (Y) {
+                        if (A.iFelN(A.MDykv, A.ikWbd)) {
+                            var d;
+                            let T = w;
+                            for (d of Y.split(/\./)) T = T[d];
+                            return g.getOwnPropertyDescriptor(T, A) ? "1" : "0";
+                        }
+                        return "" + Y;
+                    }
+                })("HTMLElement.prototype", "innerText"), W("HTMLElement.prototype", "onload"), W("HTMLElement.prototype", "onerror"), W("HTMLElement.prototype", "onmousedown"), W("HTMLElement.prototype", "onmouseup"), W("HTMLElement.prototype", "onmousemove"), W("HTMLElement.prototype", "onclick"), W("Element.prototype", "innerHTML"), W("HTMLScriptElement.prototype", "src"), W("HTMLImageElement.prototype", "src"), W("HTMLLinkElement.prototype", "href"), W("HTMLLinkElement.prototype", "type"), W("HTMLLinkElement.prototype", "rel"), W("MessagePort.prototype", "onmessage") ], G = bid, q = null;
+                (W = window?.performance?.memory) && (q = {
+                    totalJSHeapSize: W.totalJSHeapSize,
+                    usedJSHeapSize: W.usedJSHeapSize,
+                    jsHeapSizeLimit: W.jsHeapSizeLimit
+                });
+                var W = Date.now() - window.gid, Z = H && Date.now() - H;
+                await ((z, S) => z(S, {
+                    body: JSON.stringify({
+                        esid: E,
+                        uaid: O,
+                        wkid: I,
+                        muid: q,
+                        ltid: W,
+                        dtid: Z,
+                        ctid: "frame"
+                    }),
+                    method: "POST",
+                    keepalive: !0
+                }))(fetch, "/report/" + G);
             }
         }
-    };
-})();
+    }
+};
