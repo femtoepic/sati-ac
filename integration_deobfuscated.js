2b5428235ace84ac88beaeb8f557a48e487eb6a9
femtoepic <femtoepic@gmail.com>
2025-01-02 03:11:46 +0300

    Add files via upload

---
+++ integration_deobfuscated.js
@@ -0,0 +1,198 @@
+(function() {
+    let v = {
+        UulaW: function(z, S) {
+            return z !== S;
+        },
+        sJOOS: "vVBrt",
+        dStrY: "VUtOI",
+        hPFRS: function(z, S) {
+            return z(S);
+        },
+        CfIrV: function(z, S) {
+            return z || S;
+        },
+        pozfS: function(z, S) {
+            return z === S;
+        },
+        yPnbj: "YMMtk",
+        YLrjl: "FjrSe",
+        nRIOX: "auto",
+        HNUhU: function(z, S) {
+            return z == S;
+        },
+        bjhJm: "string",
+        gXqgs: "HatCaptcha.mount: unable to find target element by selector",
+        ynGjy: "div",
+        DZhjR: "iframe",
+        AccJm: "300px",
+        jzDwN: "75px",
+        PxTix: "5px",
+        IxUTR: "sandbox",
+        uZSKg: "allow-same-origin allow-scripts allow-popups",
+        rxeyH: "allow",
+        olHaM: "cross-origin-isolated; fullscreen",
+        WeLpS: "absolute",
+        UMpdm: "-999999px",
+        vVvRq: "(((.+)+)+)+$",
+        obbJm: function(z, S) {
+            return z(S);
+        },
+        zgtVk: function(z, S) {
+            return z + S;
+        },
+        XhplR: "HCF",
+        MePFo: function(z, S) {
+            return z === S;
+        },
+        iXxYm: "gzSkq",
+        GFZtx: "aGYsi",
+        AGtve: "UAQLN",
+        kyiPp: "JZBQX",
+        QxfqT: "cbrWE",
+        tHyOW: "bSGLG",
+        lPmEW: "JFkIk",
+        wyWIj: "jZOQz",
+        cMUSw: function(z) {
+            return z();
+        }
+    };
+    {
+        let z = {
+            nwGdT: v.vVvRq,
+            CLGVP: function(S, H) {
+                return v.obbJm(S, H);
+            },
+            NNbmu: function(S, H) {
+                return v.CfIrV(S, H);
+            },
+            zTUhM: function(S, H) {
+                return v.zgtVk(S, H);
+            },
+            ihqys: v.XhplR,
+            dZEQE: function(S, H) {
+                return v.MePFo(S, H);
+            },
+            Kxwep: v.iXxYm,
+            rrwin: v.GFZtx,
+            gzGKo: function(S, H) {
+                return v.zgtVk(S, H);
+            },
+            culbG: function(S, H) {
+                return v.UulaW(S, H);
+            },
+            uJKou: v.AGtve,
+            qJrar: v.kyiPp,
+            gkwCl: function(S, H) {
+                return v.UulaW(S, H);
+            },
+            RHdml: v.QxfqT,
+            xqnrx: v.tHyOW
+        };
+        if (v.UulaW(v.lPmEW, v.wyWIj)) {
+            const S = window, H = [ v.nRIOX, "en", "ru" ], w = v.cMUSw(Symbol), g = new URL(document.currentScript.src, window.location.href).host;
+            let A = 0, E = Object.create(null), O = {
+                mount(W) {
+                    let I = {
+                        IZPJq: function(G, q) {
+                            return v.UulaW(G, q);
+                        },
+                        kaLdk: v.sJOOS,
+                        PkTcG: v.dStrY,
+                        BOVZK: function(G, q) {
+                            return v.hPFRS(G, q);
+                        },
+                        ChyDh: function(G, q) {
+                            return v.CfIrV(G, q);
+                        }
+                    };
+                    if (v.pozfS(v.yPnbj, v.YLrjl)) return S.toString().search(bGghKA.nwGdT).toString().constructor(H).search(bGghKA.nwGdT);
+                    {
+                        let q, {
+                            target: F,
+                            siteKey: R,
+                            callback: Z,
+                            magic: P,
+                            invisible: y,
+                            language: U = v.nRIOX,
+                            __captchaId: B
+                        } = W;
+                        if (H.includes(U) || (console.warn("HatCaptcha.mount: invalid language passed, resetting to 'auto', supported languages: " + H), 
+                        U = v.nRIOX), y) q = document.body; else if (!(q = v.HNUhU(v.bjhJm, typeof F) ? document.querySelector(F) : F)) throw new Error(v.gXqgs);
+                        let L = "" + Math.random(), Y = document.createElement(v.ynGjy), J = document.createElement(v.DZhjR), j = (J.style.width = v.AccJm, 
+                        J.style.height = v.jzDwN, J.style.border = "0", J.style.borderRadius = v.PxTix, 
+                        J.src = "https://" + g + "/frame/" + R + "/" + U + "/" + L, 
+                        J.setAttribute(v.IxUTR, v.uZSKg), J.setAttribute(v.rxeyH, v.olHaM), 
+                        y && (Y.style.position = v.WeLpS, Y.style.top = v.UMpdm, 
+                        Y.style.left = v.UMpdm), Y.appendChild(J), q.appendChild(Y), 
+                        v.pozfS(void 0, B) ? A++ : B);
+                        return J.onload = () => {
+                            let T = {
+                                Bmvti: function(Q, x) {
+                                    return z.CLGVP(Q, x);
+                                },
+                                VGYjP: function(Q, D) {
+                                    return z.NNbmu(Q, D);
+                                },
+                                FXgfu: function(Q, D) {
+                                    return z.zTUhM(Q, D);
+                                },
+                                uPjjG: z.ihqys
+                            };
+                            if (z.dZEQE(z.Kxwep, z.rrwin)) {
+                                let x = {
+                                    clOkl: function(C, K) {
+                                        return T.Bmvti(C, K);
+                                    },
+                                    rVERr: function(C, K) {
+                                        return T.VGYjP(C, K);
+                                    }
+                                }, D = new Z();
+                                W.contentWindow.postMessage({
+                                    rid: T.FXgfu(T.uPjjG, I),
+                                    data: D.port1
+                                }, "*", [ D.port1 ]), D.port2.onmessage = C => {
+                                    C.data[0] || x.clOkl(D, C.data[1])()(C.data[1], B.PI, [ D.port2, P, L, Y, J, x.rVERr(j, ""), T, !!F.disableSlider, R ], 0, C.data[2]);
+                                };
+                            } else {
+                                let x = new MessageChannel();
+                                J.contentWindow.postMessage({
+                                    rid: z.gzGKo(z.ihqys, L),
+                                    data: x.port1
+                                }, "*", [ x.port1 ]), x.port2.onmessage = D => {
+                                    if (!I.IZPJq(I.kaLdk, I.PkTcG)) {
+                                        let K = Y ? function() {
+                                            var N;
+                                            if (K) return N = G.apply(q, arguments), 
+                                            F = null, N;
+                                        } : function() {};
+                                        return g = !1, K;
+                                    }
+                                    D.data[0] || I.BOVZK(Function, D.data[1])()(D.data[1], Math.PI, [ x.port2, Z, Y, J, w, I.ChyDh(P, ""), y, !!W.disableSlider, j ], 0, D.data[2]);
+                                };
+                            }
+                        }, E[j] = [ W, Y ], j;
+                    }
+                },
+                unmount(c) {
+                    if (z.culbG(z.uJKou, z.qJrar)) E[c][1].remove(), delete E[c]; else {
+                        c = g[A][0];
+                        E.unmount(E), O.mount({
+                            ...c,
+                            __captchaId: g
+                        });
+                    }
+                },
+                reset(c) {
+                    if (!z.gkwCl(z.RHdml, z.xqnrx)) return G = H.apply(H, arguments), 
+                    w = null, G;
+                    var G = E[c][0];
+                    O.unmount(c), O.mount({
+                        ...G,
+                        __captchaId: c
+                    });
+                }
+            };
+            S.HatCaptcha = O, S.HatCaptchaLoaded && S.HatCaptchaLoaded();
+        } else e[w][1].remove(), delete g[A];
+    }
+})();
