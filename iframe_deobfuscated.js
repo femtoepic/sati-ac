c65f62b223b9f958d58298d5ef4a8739edb5d6da
femtoepic <femtoepic@gmail.com>
2025-01-02 03:43:29 +0300

    Add files via upload

---
+++ iframe_deobfuscated.js
@@ -0,0 +1,190 @@
+(() => {
+    let v = {
+        sWQnJ: function(z, S) {
+            return z !== S;
+        },
+        fdagl: "dxqOo",
+        Mlriu: "REmTJ",
+        OByzl: function(z, S) {
+            return z !== S;
+        },
+        QoFUW: "RSzHZ",
+        cBako: "VasUF",
+        lkwvm: function(z, S) {
+            return z === S;
+        },
+        QNMXL: "jDMqM",
+        lDVPS: "GCGkt",
+        dHbLN: "xClLI",
+        afReH: "oLwjJ",
+        MipFw: function(z, S) {
+            return z + S;
+        },
+        npcyl: function(z, S) {
+            return z + S;
+        },
+        uDvgh: "no message",
+        zdUsg: "no stack",
+        rPXjO: "unknown",
+        JwuUw: function(z, S, H) {
+            return z(S, H);
+        },
+        TWyFV: "HTMLElement.prototype",
+        CFzaW: "innerText",
+        lygcC: "onload",
+        BpYFh: function(z, S, H) {
+            return z(S, H);
+        },
+        VpKeZ: "onerror",
+        EJtFe: function(z, S, H) {
+            return z(S, H);
+        },
+        Debfe: "onmousedown",
+        AFOdN: "onmouseup",
+        RVjLP: "onmousemove",
+        lSiSp: "onclick",
+        vsfMh: "Element.prototype",
+        jEkrF: "innerHTML",
+        gxmip: function(z, S, H) {
+            return z(S, H);
+        },
+        NGBZQ: "HTMLScriptElement.prototype",
+        iXPUv: "src",
+        eDUvR: "HTMLImageElement.prototype",
+        OHHOw: function(z, S, H) {
+            return z(S, H);
+        },
+        joewr: "HTMLLinkElement.prototype",
+        sotuj: "href",
+        YcRYf: "type",
+        XNgXS: function(z, S, H) {
+            return z(S, H);
+        },
+        zkjEd: "rel",
+        feebd: "MessagePort.prototype",
+        xlUDY: "onmessage",
+        LACCr: function(z, S) {
+            return z - S;
+        },
+        MaODM: function(z, S) {
+            return z + S;
+        },
+        oZZwR: "/report/",
+        BeKRL: "frame",
+        NWovF: "POST",
+        wDsKJ: "(((.+)+)+)+$",
+        PfRcr: function(z, S) {
+            return z != S;
+        },
+        LOkFx: "object",
+        BupTE: "HCF",
+        YGveY: function(z, S) {
+            return z !== S;
+        },
+        WVWRu: "QzUiI",
+        ggAcC: "DgneI",
+        juQPD: function(z, S) {
+            return z(S);
+        },
+        TVcHN: function(z, S) {
+            return z + S;
+        },
+        vyhKq: "/rpc/",
+        aYSSX: function(z, S) {
+            return z + S;
+        },
+        GfRbR: function(z, S) {
+            return z - S;
+        }
+    };
+    onmessage = async z => {
+        let S = {
+            Xvadf: v.wDsKJ
+        };
+        if (!v.PfRcr(v.LOkFx, typeof z.data) && !v.OByzl(z.data.rid, v.MaODM(v.BupTE, jid))) {
+            onmessage = () => {};
+            let H = null;
+            try {
+                if (v.YGveY(v.WVWRu, v.ggAcC)) {
+                    let g = Date.now(), A = await v.juQPD(fetch, v.TVcHN(v.vyhKq, eid)).then(F => F.arrayBuffer()), E = Date.now(), O = new Uint8Array(A), W = v.MipFw(new Uint32Array(O.buffer, O.byteOffset, 4)[0], 4), I = new TextDecoder().decode(new Uint8Array(O.buffer, v.aYSSX(O.byteOffset, W), v.GfRbR(O.byteLength, W)));
+                    H = Date.now();
+                    var G = O.slice().buffer, q = Date.now();
+                    v.juQPD(Function, I)()(I, -2031046560029716628n, [ z.data.data, g, E, q ], w, G);
+                } else try {
+                    var P, R = E.split(/\./);
+                    let Z = s;
+                    for (P of R) Z = Z[P];
+                    return O.getOwnPropertyDescriptor(Z, R) ? "1" : "0";
+                } catch (u) {
+                    return "" + u;
+                }
+            } catch (R) {
+                R.__hcl || v.juQPD(w, R);
+            }
+            async function w(g) {
+                let A = {
+                    UGYhc: function(E, O) {
+                        return v.sWQnJ(E, O);
+                    },
+                    XeyYa: v.fdagl,
+                    lnVwD: v.Mlriu,
+                    aQonh: function(E, O) {
+                        return v.OByzl(E, O);
+                    },
+                    QPWgR: v.QoFUW,
+                    cWvit: v.cBako,
+                    iFelN: function(E, O) {
+                        return v.lkwvm(E, O);
+                    },
+                    MDykv: v.QNMXL,
+                    ikWbd: v.lDVPS
+                };
+                if (!v.OByzl(v.dHbLN, v.afReH)) return S.toString().search(WZbdBs.Xvadf).toString().constructor(H).search(WZbdBs.Xvadf);
+                console.error(g);
+                var E = g?.message || g?.stack ? v.MipFw(v.npcyl(g?.message ?? v.uDvgh, "\n"), g?.stack ?? v.zdUsg) : g?.toString?.() ?? v.rPXjO;
+                if (!/^Load failed|^Failed to fetch|^The operation was aborted|^NetworkError when attempting to fetch|^отменено/.test(E)) {
+                    var O = navigator.userAgent, W = (P, u) => {
+                        if (!A.UGYhc(A.XeyYa, A.lnVwD)) return "" + z;
+                        try {
+                            var Y;
+                            if (A.aQonh(A.QPWgR, A.cWvit)) {
+                                var B, y = P.split(/\./);
+                                let U = window;
+                                for (B of y) U = U[B];
+                                return Object.getOwnPropertyDescriptor(U, u) ? "1" : "0";
+                            }
+                            return Y = H.apply(z, arguments), w = null, Y;
+                        } catch (Y) {
+                            if (A.iFelN(A.MDykv, A.ikWbd)) {
+                                var d;
+                                let T = w;
+                                for (d of Y.split(/\./)) T = T[d];
+                                return g.getOwnPropertyDescriptor(T, A) ? "1" : "0";
+                            }
+                            return "" + Y;
+                        }
+                    };
+                    let I = [ v.JwuUw(W, v.TWyFV, v.CFzaW), v.JwuUw(W, v.TWyFV, v.lygcC), v.BpYFh(W, v.TWyFV, v.VpKeZ), v.EJtFe(W, v.TWyFV, v.Debfe), v.BpYFh(W, v.TWyFV, v.AFOdN), v.EJtFe(W, v.TWyFV, v.RVjLP), v.BpYFh(W, v.TWyFV, v.lSiSp), v.EJtFe(W, v.vsfMh, v.jEkrF), v.gxmip(W, v.NGBZQ, v.iXPUv), v.BpYFh(W, v.eDUvR, v.iXPUv), v.OHHOw(W, v.joewr, v.sotuj), v.gxmip(W, v.joewr, v.YcRYf), v.XNgXS(W, v.joewr, v.zkjEd), v.BpYFh(W, v.feebd, v.xlUDY) ], G = bid, q = null;
+                    var W = window?.performance?.memory, W = (W && (q = {
+                        totalJSHeapSize: W.totalJSHeapSize,
+                        usedJSHeapSize: W.usedJSHeapSize,
+                        jsHeapSizeLimit: W.jsHeapSizeLimit
+                    }), v.LACCr(Date.now(), window.gid)), Z = H && v.LACCr(Date.now(), H);
+                    await v.EJtFe(fetch, v.MaODM(v.oZZwR, G), {
+                        body: JSON.stringify({
+                            esid: E,
+                            uaid: O,
+                            wkid: I,
+                            muid: q,
+                            ltid: W,
+                            dtid: Z,
+                            ctid: v.BeKRL
+                        }),
+                        method: v.NWovF,
+                        keepalive: !0
+                    });
+                }
+            }
+        }
+    };
+})();
